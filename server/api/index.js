a4bfd5a760f2a3022949a2012479f9d43620c852
Jess B <jessica.bracht@gracehopper.com>
2021-04-19 12:35:43 -0400

    (feat) Add route for GET /potato

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,10 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
+router.get('/potato', (req, res) => {
+  res.send('taters');
+})
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
